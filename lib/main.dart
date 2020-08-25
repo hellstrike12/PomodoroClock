d28239c8e6dc44c7223ded04972c7172bf5cd1c1
Bryan Souza <bryan@pop-os.localdomain>
2020-08-25 09:04:30 -0300

    Add progress bar & bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,38 @@ class MyApp extends StatelessWidget {
       title: 'Pomodoro Clock',
       home: Scaffold(
         appBar: AppBar(title: Text('Pomodoro Clock')),
-        body: Center(child: Body()),
+        body: Center(child: Clock()),
+        floatingActionButton: IconButton(
+          // Create activity
+          icon: Icon(
+            Icons.add_circle_rounded,
+            size: 60,
+            color: Colors.blue,
+          ),
+          padding: EdgeInsets.all(4.0),
+          onPressed: null,
+        ),
       ),
     );
   }
 }
 
 //----------------------------------- BODY -------------------------------------
-class Body extends StatefulWidget {
+class Clock extends StatefulWidget {
   @override
-  _BodyState createState() => _BodyState();
+  _ClockState createState() => _ClockState();
 }
 
-class _BodyState extends State<Body> {
+class _ClockState extends State<Clock> {
   int _min = 25;
   int _sec = 0;
   bool _breakTime = false;
   bool _pause = true;
+  Color _bgBreakColor = Colors.lightGreen[200];
+  Color _fgBreakColor = Colors.green;
+
+  Color _bgWorkColor = Colors.lightBlue[200];
+  Color _fgWorkColor = Colors.blue;
 
   void _tick() {
     if (mounted && !_pause) {
@@ -69,18 +84,44 @@ class _BodyState extends State<Body> {
     _stopTimer();
     _min = 25;
     _sec = 0;
+    _breakTime = false;
     if (mounted) setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      child: Text(
-        '${_min > 9 ? _min : "0$_min"}:${_sec > 9 ? _sec : "0$_sec"}',
-        style: TextStyle(fontSize: 50),
+    return Container(
+      child: Stack(
+        alignment: Alignment(0, 0),
+        children: [
+          Container(
+            child: CircularProgressIndicator(
+              value: _breakTime
+                  ? ((_min * 60) + _sec) / 300
+                  : ((_min * 60) + _sec) / 1500,
+              backgroundColor: (_breakTime ? _bgBreakColor : _bgWorkColor),
+              valueColor: AlwaysStoppedAnimation(
+                  _breakTime ? _fgBreakColor : _fgWorkColor),
+            ),
+            height: 165,
+            width: 165,
+          ),
+          FlatButton(
+            child: Text(
+              '${_min > 9 ? _min : "0$_min"}:${_sec > 9 ? _sec : "0$_sec"}',
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+            onPressed: () => _pause ? _startTimer() : _stopTimer(),
+            onLongPress: () => _resetTimer(),
+            shape: CircleBorder(),
+            height: 160,
+            color: Colors.white,
+          ),
+        ],
       ),
-      onPressed: () => _pause ? _startTimer() : _stopTimer(),
-      onLongPress: () => _resetTimer(),
+      alignment: Alignment.center,
     );
   }
 }
