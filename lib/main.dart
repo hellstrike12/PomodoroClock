ff9df11e445835f5698e8b95e0e1536d24a76bf9
Bryan Souza <bryan@pop-os.localdomain>
2020-08-28 15:48:47 -0300

    Add .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,74 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:firebase_database/firebase_database.dart';
 
 void main() => runApp(MyApp());
 
+//--------------------------------- ROOT ---------------------------------------
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pomodoro Clock',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Pomodoro Clock')),
-        body: Center(child: Clock()),
-        floatingActionButton: IconButton(
-          // Create activity
-          icon: Icon(
-            Icons.add_circle_rounded,
-            size: 60,
-            color: Colors.blue,
-          ),
-          padding: EdgeInsets.all(4.0),
-          onPressed: null,
+      home: Home(),
+    );
+  }
+}
+
+//------------------------------------------------------------------------------
+//---------------------------------- HOME --------------------------------------
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pomodoro Clock')),
+      body: Center(child: Clock()),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Column(
+                children: [
+                  // Titulo
+                  Text(
+                    'Pomodoro Clock',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  Text(
+                    // Subtitulo
+                    "Simple pomodoro clock\nusing Google's Flutter API",
+                    style: TextStyle(color: Colors.grey[200]),
+                    textAlign: TextAlign.end,
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('Minhas atividades'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Activities(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-//----------------------------------- BODY -------------------------------------
+//------------------------------------------------------------------------------
+//--------------------------------- CLOCK --------------------------------------
 class Clock extends StatefulWidget {
   @override
   _ClockState createState() => _ClockState();
@@ -37,6 +79,7 @@ class _ClockState extends State<Clock> {
   int _sec = 0;
   bool _breakTime = false;
   bool _pause = true;
+
   Color _bgBreakColor = Colors.lightGreen[200];
   Color _fgBreakColor = Colors.green;
 
@@ -103,21 +146,21 @@ class _ClockState extends State<Clock> {
               valueColor: AlwaysStoppedAnimation(
                   _breakTime ? _fgBreakColor : _fgWorkColor),
             ),
-            height: 165,
-            width: 165,
+            height: 220,
+            width: 220,
           ),
           FlatButton(
             child: Text(
               '${_min > 9 ? _min : "0$_min"}:${_sec > 9 ? _sec : "0$_sec"}',
               style: TextStyle(
-                fontSize: 50,
+                fontSize: 70,
               ),
             ),
             onPressed: () => _pause ? _startTimer() : _stopTimer(),
             onLongPress: () => _resetTimer(),
             shape: CircleBorder(),
-            height: 160,
             color: Colors.white,
+            height: 250,
           ),
         ],
       ),
@@ -125,3 +168,109 @@ class _ClockState extends State<Clock> {
     );
   }
 }
+
+//------------------------------------------------------------------------------
+//------------------------------- ACTIVITIES -----------------------------------
+class Activities extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minhas Atividades'),
+      ),
+      body: Center(
+        child: Text('Teste'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, size: 30),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return PopUp();
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+//------------------------------------------------------------------------------
+//---------------------------------- POP UP ------------------------------------
+class PopUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Column(
+        // HEADING
+        children: [
+          // Heading
+          PopUpHead(),
+          PopUpBody()
+        ],
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+    );
+  }
+}
+
+//------------------------------------------------------------------------------
+//----------------------------- POP-UP HEAD ------------------------------------
+class PopUpHead extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'Criar nova atividade',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      width: 300,
+    );
+  }
+}
+
+//------------------------------------------------------------------------------
+//----------------------------- POP-UP BODY ------------------------------------
+class PopUpBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: "Nome da atividade"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Nome da atividade não especificado";
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
+        padding: EdgeInsets.all(15),
+      ),
+    );
+  }
+}
+//------------------------------------------------------------------------------
